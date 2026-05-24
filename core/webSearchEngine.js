a4782172aef3b81d493f6688d26e1c57ac0612a8
caua-vosc <caua_viniciusosc12@outlook.com>
2026-05-24 17:32:06 -0300

    Add webSearch function for various API integrations

---
+++ core/webSearchEngine.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+export async function webSearch(query) {
+
+  try {
+
+    // PREPARADO PARA:
+    // Tavily
+    // SerpAPI
+    // Google Search
+    // Weather APIs
+    // Maps APIs
+
+    return {
+
+      success: true,
+
+      results: []
+
+    };
+
+  } catch {
+
+    return {
+
+      success: false,
+
+      results: []
+
+    };
+
+  }
+
+}
